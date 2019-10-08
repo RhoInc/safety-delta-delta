c26dd42e4db48c51fd3e24c95b9f73bff0d5e609
jwildfire <jwildfire@gmail.com>
2019-10-08 09:52:14 -0300

    add 2nd example. fix #3

---
+++ test-page/example1/index.js
@@ -0,0 +1,15 @@
+const settings =   {
+      studyday_col: 'LBDY',
+      value_col: 'LBSTRESN',
+      measure_col: 'LBTEST',
+      normal_col_low: 'LBSTNRLO',
+      normal_col_high: 'LBSTNRHI',
+      visit_col: 'VISIT',
+      visitn_col: 'VISITNUM',
+      visits: { baseline: [], comparison: ["WEEK 26"], stat: 'mean' }
+    //  group_cols: 'ARM',
+  }
+const chart = safetyDeltaDelta('#container', settings);
+d3.csv('https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/sdtm/cdisc-pilot-01/lb.csv', function(data) {
+    chart.init(data);
+});
